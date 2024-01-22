c24258a9e28dd07e7d97a5b1bf87ee437ae79554
Mohnish <mohnish.a007@gmail.com>
2024-01-22 00:32:30 +0000

    encrypt perfectly working

--- encrypt.py
+++ encrypt.py
@@ -1,7 +1,8 @@
+import os
 import hashlib 
 from dotenv import load_dotenv,dotenv_values
 
 load_dotenv()
 password = os.getenv("MY_SECRET")
-print(hashlib.sha256(password)).hexdigest()
+print(hashlib.sha256(password.encode('utf-8')).hexdigest())
 print(hashlib.algorithms_available)
